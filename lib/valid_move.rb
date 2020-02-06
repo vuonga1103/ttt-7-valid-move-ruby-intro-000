22007e2f9a9f4da7cb62610e219296ad2f56674c
Anh Vuong <vuonga1103@yahoo.com>
2020-02-06 21:12:48 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,5 @@
 def valid_move?(board, index)
-  if ((board[index] == "") || (board[index] == " ") || (board[index] == nil))
+  if (!(board[index] == "") || (board[index] == " ") || (board[index] == nil))
     return false
   elsif (index < 0 || index > 8)
     return false
