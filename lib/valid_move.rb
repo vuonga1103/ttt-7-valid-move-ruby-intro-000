47807f97bc3edecd37fa0dd7ff83c90b74abb17d
Anh Vuong <vuonga1103@yahoo.com>
2020-02-06 21:11:59 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,10 @@
-# code your #valid_move? method here
-
+def valid_move?(board, index)
+  if ((board[index] == "") || (board[index] == " ") || (board[index] == nil))
+    return false
+  elsif (index < 0 || index > 8)
+    return false
+  end
+  return true
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
